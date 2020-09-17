f362bcfe7487cbc63fadb958c2d9083c53f42c74
autohome <administrator@at5274-dlp.corp.autohome.inc>
2020-09-17 14:51:29 +0800

    source code

---
+++ iOSWheels/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSWheels
+//
+//  Created by autohome on 2020/9/17.
+//  Copyright © 2020 baigang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
